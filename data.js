0cd04f69a2e6f9060dc0fc3e85547b168b7a0cf7
RebeccaChenSG <92525436+RebeccaChenSG@users.noreply.github.com>
2022-05-12 15:06:59 +0800

    First commit
    My first WebMaker Tour

---
+++ data.js
@@ -0,0 +1,158 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.3680653875203639,
+        "pitch": 0,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1495533470746544,
+          "pitch": -0.11866758336086036,
+          "rotation": 7.0685834705770345,
+          "target": "1-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0480278904840894,
+          "pitch": 0.24098817417762497,
+          "title": "Welcome to NUS Design of Design and Engineering",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-engineering-auditorium",
+      "name": "Engineering Auditorium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.21348509103501634,
+        "pitch": -0.10235673185704641,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1005073468698594,
+          "pitch": -0.0005357065785354109,
+          "rotation": 0.7853981633974483,
+          "target": "2-engineering-auditorium-atrium"
+        },
+        {
+          "yaw": 3.0806150857881063,
+          "pitch": 0.10717403330639108,
+          "rotation": 4.71238898038469,
+          "target": "0-along-engineering-drive-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0310689217332563,
+          "pitch": -0.25868364193837046,
+          "title": "Building interdisciplinary campus community through public art",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.511814556606514,
+        "pitch": -0.000019136133117214627,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2900067412258487,
+          "pitch": 0.38276257909251044,
+          "rotation": 6.283185307179586,
+          "target": "1-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.511814556606514,
+          "pitch": -0.000019136133117214627,
+          "title": "<font color=\"#000000\" face=\"sofia-pro, sans-serif\"><span style=\"font-size: 19.2px; letter-spacing: -0.33px; background-color: rgb(255, 255, 255);\">The coll</span></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.511814556606514,
+          "pitch": -0.000019136133117214627,
+          "title": "The college is home to 11 departments in 2 schools",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Engineering WebMakerTour 120522",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
